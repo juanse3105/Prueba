e153332df0832797f95ed70d4303de2498401bf0
Jorge Prada <pradaje2004@gmail.com>
2016-11-09 21:03:36 -0500

    Mi primer commit

---
+++ ValidarCampos.vb
@@ -0,0 +1,19 @@
+﻿Public Class ValidarCampos
+
+    Class ValidarCampos()
+
+    End Class
+    Public Function numeros(e As KeyPressEventArgs) As KeyPressEventArgs
+        If ((e.KeyChar < "0" Or e.KeyChar > "9") And Asc(e.KeyChar) <> 13 And Asc(e.KeyChar) <> 8) Then
+            e.KeyChar = vbNullString
+        End If
+        numeros = e
+    End Function
+
+    Public Function Enter(e As KeyEventArgs) As KeyEventArgs
+        If (e.KeyCode = Keys.Enter) Then
+            SendKeys.Send("{TAB}")
+        End If
+        Enter = e
+    End Function
+End Class
